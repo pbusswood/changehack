047eecc2e0a35d6bb2e2df2152cc10fc58efa71d
Parker Busswood <parkerbusswood@gmail.com>
2014-09-01 19:20:52 -0700

    Updating .gitignore, adding nav menu button

--- js/script.js
+++ js/script.js
@@ -147,6 +147,11 @@ $(function() {
      });
   });
 
+  $('nav .btn').click(function() {
+    $(this).toggleClass('close');
+    $(this).parent().toggleClass('open');
+  });
+
   // Email validation
   function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
